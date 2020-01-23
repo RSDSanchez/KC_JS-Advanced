8a2017a249994fc30385fb4f6d47156931cb89d7
geuv <geuvadan@gmail.com>
2020-01-23 06:16:20 +0100

    empezado maquetado filtros

---
+++ js/global.js
@@ -0,0 +1,3 @@
+export const hide = element => element.classList.add('hide');
+
+export const show = element => element.classList.remove('hide');
--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 import api from './api.js';
+import { hide, show } from './global.js';
 
 const { getBeers, getBeerById } = api();
+const searchButton = document.getElementById('search-button');
+const searchField = document.getElementById('searcher');
+const dateButton = document.getElementById('date-button');
+const dateField = document.getElementById('date-field');
 
 const beerTemplate = (beer, index) => {
-    return `
+   return `
    <article class="item item${index + 1}">
       <div class="description">
          <div class="info">
@@ -29,30 +34,52 @@ const beerTemplate = (beer, index) => {
 };
 
 const printBeers = beers => {
-    const sectionContainer = document.querySelector('.section-container');
-    const beersHTML = beers
-        .slice(0, 6)
-        .map((beer, index) => {
-            return beerTemplate(beer, index);
-        })
-        .join('');
-    sectionContainer.innerHTML = beersHTML;
-}
+   const sectionContainer = document.querySelector('.section-container');
+   const beersHTML = beers
+      .slice(0, 6)
+      .map((beer, index) => {
+         return beerTemplate(beer, index);
+      })
+      .join('');
+   sectionContainer.innerHTML = beersHTML;
+};
 
 const showBeers = async keyword => {
-    const beers = await getBeers(keyword);
-    console.log(beers);
-    printBeers(beers);
+   const beers = await getBeers(keyword);
+   console.log(beers);
+   printBeers(beers);
 };
 
 showBeers();
 
 const filterByDate = async date => {
-    const beers = await getBeers();
-    const filteredBeers = beers.filter(beer => beer.firstBrewed === date);
-    console.log(filteredBeers);
-    printBeers(filteredBeers);
-}
+   const beers = await getBeers();
+   const filteredBeers = beers.filter(beer => beer.firstBrewed === date);
+   console.log(filteredBeers);
+   printBeers(filteredBeers);
+};
+
+searchButton.addEventListener('click', evt => {
+   evt.preventDefault();
+   if (!searchField.value) {
+      hide(dateField);
+      show(searchField);
+   } else {
+      showBeers(searchField.value);
+      searchField.value = '';
+      hide(searchField);
+   }
+});
+
+dateButton.addEventListener('click', evt => {
+   evt.preventDefault();
+   if (!dateField.value) {
+      hide(searchField);
+      show(dateField);
+   } else {
+      console.log(searchField.value);
+   }
+});
 
 // filterByDate('04/2013');
 
